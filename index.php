caf2616cc6b142c5dad91b1cb96e626cf69e5c80
Kenza <a.kenza29@gmail.com>
2024-06-20 11:23:34 +0200

    suite exo

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ if(isset($_GET['action'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/css/uikit.min.css" />
     
     <title>Ajout produit</title>
@@ -49,7 +50,7 @@ if(isset($_GET['action'])){
             </div>
             <div>
                 <ul class="navbar-right">
-                    <li><a href="http://premiere-application-web-php.test/recap.php">PANIER</a></li>
+                    <li><a href="http://premiere-application-web-php.test/recap.php">PANIER</a><i class="fa-solid fa-cart-shopping"></i></li>
                 </ul>
             </div>
         </div>
@@ -57,39 +58,47 @@ if(isset($_GET['action'])){
       
     </nav>
 
+    <div class="uk-height-medium uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light" id="image" data-src="https://images.unsplash.com/photo-1495321308589-43affb814eee?fit=crop&w=650&h=433&q=80" uk-img="loading: eager">
+    <main class="produit">
+        <div class="card">
+            <h1>Ajouter un produit</h1>
+            <form  action="traitement.php?action=addProduct" method="post">
+                <p>
+                    <label>
+                        Nom du produit : 
+                        <input type="text" name="name">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Prix du produit :
+                        <input type="number" step="any" name="price" min="0" required>
+                        
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Quantité désirée:
+                        <input type="number" name="qtt" value="1" min="1" required> 
+                    </label>
+                </p>
+                <p>         
+                    <input id="submit" class="uk-button uk-button-secondary uk-width-1-1" type="submit" name="submit" value="Ajouter le produit">
+                </p>
+            </form>
+        </div>
        
+    </main>
+   
+    </div>
 
-
+     <div class="article">
+        <i class="fa-solid fa-cart-shopping" id="panier"></i>
+        <p class="total-qqt">Quantité totale d'articles : <?php echo $totalQuantite; ?></p>
+     </div>  
     
-    <h1 class="uk-heading-line uk-text-center">Ajouter un produit</h1>
-    <form class="uk-card uk-card-secondary uk-card-hover uk-card-body uk-light" action="traitement.php?action=addProduct" method="post">
-        <p>
-            <label>
-                Nom du produit : 
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit :
-                <input type="number" step="any" name="price" min="0" required>
-                
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée:
-                <input type="number" name="qtt" value="1" min="1" required> 
-            </label>
-        </p>
-        <p>         
-            <input class="uk-button uk-button-default" type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
     
- 
-    <p>Quantité totale d'articles : <?php echo $totalQuantite; ?></p>
-<?php if(!empty($message)) {?>
+    <?php if(!empty($message)) {?>
     <div class="message" id="message" >  <?php echo $message; }?></div>
 
 
--- recap.php
+++ recap.php
@@ -8,18 +8,40 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/css/uikit.min.css" />
     
     <title>Récapitulatif des produits</title>
 </head>
 <body>
-<a href="http://premiere-application-web-php.test/index.php">index.php</a>
+<nav class="navbar">
+        <div class="navbar-centre">
+            <div>
+                <ul class="ul-navbar">
+                    <li><a href="#">FEMME</a></li>
+                    <li><a href="#">HOMME</a></li>
+                    <li><a href="#">ENFANTS</a></li>
+                    <li><a href="#">HOME</a></li>
+                    
+                </ul>
+            </div>
+            <div>
+                <ul class="navbar-right">
+                    <li><a href="http://premiere-application-web-php.test/recap.php">PANIER</a><i class="fa-solid fa-cart-shopping"></i></li>
+                </ul>
+            </div>
+        </div>
+       
+      
+    </nav>
+
+
     
     <?php if(!isset($_SESSION['products']) || empty($_SESSION['products'])) {
         echo "<p> Aucun produit en session... </p>";
 
     }else{
-        echo "<table class='uk-table uk-table-hover uk-table-divider'>",
+        echo "<table >",
                 "<thead>",
                     "<tr>",
                         "<th>#</th>",
@@ -40,7 +62,7 @@ session_start();
                         
                         "<td>" .$product['name']. "</td>",
                         "<td>" .number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€ </td>",
-                        "<td><a href='traitement.php?action=down-qtt&id=$index'>-</a>" .$product['qtt']. "<a href='traitement.php?action=up-qtt&id=$index'>+</a></td>",
+                        "<td><a href='traitement.php?action=down-qtt&id=$index'class='quantite'>-</a>" .$product['qtt']. "<a href='traitement.php?action=up-qtt&id=$index' class='quantite'>+</a></td>",
                         "<td>" .number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€ </td>",
                         
                         "<td><a href='traitement.php?action=delete&id=$index'class='uk-icon-link' uk-icon='trash'></a></td>",
@@ -67,7 +89,7 @@ session_start();
 
   <a href="traitement.php?action=deleteAll">supprimer</a><br>
 
-
+  <a href="http://premiere-application-web-php.test/index.php">Poursuivre vos achats</a>
 
     
 
